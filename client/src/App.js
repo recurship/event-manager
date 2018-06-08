845ae68c4801278e4f5a1933f14a5b2aae2a2d9e
Mashhood Rastgar <mashhoodr@gmail.com>
2018-06-08 12:12:43 +0500

    (frontend): sample code adjusted to show data from API

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = { events: [] };
+        this.getData();
+    }
+
+    getData() {
+        let formData = new FormData();
+        formData.append('username', 'admin');
+        formData.append('password', '1299459ML');
+
+        return fetch(`/api/token`, {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            return response.json();
+        }).then(auth => {
+            console.log('Got the auth', auth);
+            return fetch(`/events`, {
+                accept: 'application/json',
+                headers: {
+                    'Authorization': `Bearer ${ auth.access }`
+                }
+            })
+        }).then(response => {
+            return response.json()
+        }).then(data => {
+           console.log('Got the data', data);
+           this.setState({ events: data });
+        });
+    }
+
+    render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <h1 className="App-title">Welcome to Event Manager</h1>
+            </header>
+            <div className="App-intro">
+            {this.state.events.map( event => {
+                return <div key={event.id}>{event.title} by {event.organisation}</div>
+            })}
+            </div>
+          </div>
+        );
+    }
 }
 
 export default App;
