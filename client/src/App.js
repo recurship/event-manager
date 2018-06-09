e8f1c725b365dd5c00a9eb8e8729508b553040b9
Mashhood Rastgar <mashhoodr@gmail.com>
2018-06-09 10:30:10 +0500

    frontend: added react router

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,23 @@ import {
   } from 'reactstrap';
 import AuthService from './services/auth';
 import EventService from './services/events';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom'
 
+const Home = () => (
+<div>
+    <h2>Home</h2>
+</div>
+)
+
+const About = () => (
+<div>
+    <h2>About</h2>
+</div>
+)
 
 
 class App extends Component {
@@ -55,51 +71,55 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-            <Navbar color="dark" dark exapand="true">
-              <NavbarToggler right="true" onClick={this.toggle} />
-              <NavbarBrand href="/">community-manager</NavbarBrand>
-              <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                  <NavItem>
-                    <NavLink href="/components/">About</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-                  </NavItem>
-                </Nav>
-              </Collapse>
-            </Navbar>
-            <Jumbotron>
-              <Container>
-                <Row>
-                  <Col>
-                    <h1>Welcome to Event Management for communities...</h1>
-                    <p>
-                      <Button
-                        tag="a"
-                        color="success"
-                        size="large"
-                        onClick={ this.getData.bind(this) }
-                      >
-                        Fetch events!
-                      </Button>
-                    </p>
-                  </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <div className="App-intro">
-                        {(this.state.events.length === 0) ? <div> No events found! </div> : <div></div> }
-                        {this.state.events.map( event => {
-                            return <div key={event.id}>{event.title} by {event.organisation}</div>
-                        })}
-                        </div>
-                    </Col>
-                </Row>
-              </Container>
-            </Jumbotron>
-          </div>
+            <Router>
+                <div>
+                    <Navbar color="dark" dark exapand="true">
+                    <NavbarToggler right="true" onClick={this.toggle} />
+                    <NavbarBrand href="/">community-manager</NavbarBrand>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <Link to="/">Home</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/about">About</Link>
+                        </NavItem>
+                        </Nav>
+                    </Collapse>
+                    </Navbar>
+                    <Jumbotron>
+                    <Container>
+                        <Row>
+                        <Col>
+                            <h1>Welcome to Event Management for communities...</h1>
+                            <p>
+                            <Button
+                                tag="a"
+                                color="success"
+                                size="large"
+                                onClick={ this.getData.bind(this) }
+                            >
+                                Fetch events!
+                            </Button>
+                            </p>
+                        </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <div className="App-intro">
+                                {(this.state.events.length === 0) ? <div> No events found! </div> : <div></div> }
+                                {this.state.events.map( event => {
+                                    return <div key={event.id}>{event.title} by {event.organisation}</div>
+                                })}
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                    </Jumbotron>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                </div>
+          </Router>
         );
     }
 }
