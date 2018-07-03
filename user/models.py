f95b53aa1473862d0bf62d0d1ec4f454781544e8
usman-hussain <usmanh@marketlytics.com>
2018-07-03 16:45:09 +0500

    whitespaces issues fixed

--- user/models.py
+++ user/models.py
@@ -7,8 +7,8 @@ from django.core.validators import FileExtensionValidator
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     enable_notifications = models.BooleanField(default=True)
-    avatar = models.FileField(upload_to='user/', blank=False, null=True, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png'])])
-    
+    avatar = models.FileField(upload_to='user/', blank=False, null=True, validators=[
+                              FileExtensionValidator(allowed_extensions=['jpg', 'png'])])
 
     def __str__(self):
         return self.username
