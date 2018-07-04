92ff4c72aa5df29d02527bc4401e9502c388c162
Rahil Khurshid Ali <infernus.rk31@gmail.com>
2018-07-04 14:46:18 +0500

    Codacy issues fixes

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,16 +1,9 @@
 import { configure } from '@storybook/react';
-/*
-function loadStories() {
-  require('../src/stories');
-}
 
-configure(loadStories, module);
-*/
-
-const req = require.context('../src/stories', true, /\.js$/)
+const req = require.context('../src/stories', true, /\.js$/);
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename))
+  req.keys().forEach((filename) => req(filename));
 }
 
 configure(loadStories, module);
