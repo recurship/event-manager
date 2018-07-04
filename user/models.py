77d966fc6b0c1047574c4194ba5ded2b75c7fc75
usman-hussain <usmanh@marketlytics.com>
2018-07-04 17:11:09 +0500

    image resize for user avatar

--- user/models.py
+++ user/models.py
@@ -2,13 +2,15 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
+from PIL import Image as Img
+from django_resized import ResizedImageField
 
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     enable_notifications = models.BooleanField(default=True)
-    avatar = models.FileField(upload_to='user/', blank=False, null=True, validators=[
-                              FileExtensionValidator(allowed_extensions=['jpg', 'png'])])
+    avatar = ResizedImageField(
+        size=[300, 300], upload_to='user', blank=False, null=True)
 
     def __str__(self):
         return self.username
