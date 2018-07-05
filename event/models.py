1c2338021abfa82458fe4fa4de0f31a60ea4f703
Javaeria <javerianisar.1993@gmail.com>
2018-07-05 16:43:02 +0500

    minor change in the event/models.py

--- event/models.py
+++ event/models.py
@@ -5,8 +5,8 @@ class Event(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField()
-    startDateTime = models.DateTimeField()
-    endDateTime = models.DateTimeField()
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
     organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
 
     def __str__(self):
