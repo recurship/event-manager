3e38481052c332c7976bbd9b708f6a862a6262a1
Owais Ahmed Qureshi <apple@Apples-MacBook-Pro.local>
2018-07-07 12:31:10 +0500

    add root styles in config and complete story for login form component

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,5 +1,9 @@
 import { configure } from '@storybook/react';
 
+//root styles
+import 'bootstrap/dist/css/bootstrap.css';
+import '../src/index.css';
+
 const req = require.context('./../src/stories/', true, /\.js$/);
 
 function loadStories() {
--- client/src/stories/loginForm.js
+++ client/src/stories/loginForm.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import LoginForm from '../components/LoginForm';
+import { Container } from 'reactstrap';
+import '../containers/Login/Login.css';
 
-storiesOf('LoginForm', module).add('Login Form', () => <LoginForm />);
+storiesOf('LoginForm', module).add('Login Form', () => 
+    <Container className="login-container">
+        <LoginForm />
+    </Container>
+);
