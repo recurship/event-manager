707390539e76a026b3b761a59b791c3f075cc1fd
Owais Ahmed Qureshi <apple@Apples-MacBook-Pro.local>
2018-07-11 16:16:26 +0500

    feat: document components with story book

--- client/src/stories/EMNavbar.js
+++ client/src/stories/EMNavbar.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { EMNavbar } from "../components/EMNavbar";
-import {BrowserRouter as Router} from "react-router-dom";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { EMNavbar } from '../components/EMNavbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { withInfo } from '@storybook/addon-info';
 
-storiesOf("EMNavbar", module).add("nav bar", () => 
-    <Router>
-        <EMNavbar />
-    </Router>
-    );
+storiesOf('EMNavbar', module).add(
+	'nav bar',
+	withInfo(`
+      Main Nav Bar component 
+    
+      ~~~js
+      <EMNavbar />
+      ~~~
+    
+    `)(() => (
+		<Router>
+			<EMNavbar />
+		</Router>
+	))
+);
