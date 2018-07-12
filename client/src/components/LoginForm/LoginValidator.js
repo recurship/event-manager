0a0e649ad601a2549e7c93303a5c59e74caa9b35
saadabbasi <saad@marketlytics.com>
2018-07-12 17:27:10 +0500

    refactor: add redux-form with validator on login

---
+++ client/src/components/LoginForm/LoginValidator.js
@@ -0,0 +1,13 @@
+import { isEmail, isLowercase, isLength } from 'validator';
+
+const validate = values => {
+  const errors = {},
+    { username, password } = values;
+
+  if (!username) errors.username = '*Username Required';
+  if (!password) errors.password = '*Password Required';
+
+  return errors;
+};
+
+export default validate;
