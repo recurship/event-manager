b21aa51e8604b4cb6be41cb3dbde65c82540d4e7
Rahil Khurshid Ali <infernus.rk31@gmail.com>
2018-07-16 10:02:02 +0500

    feat: added enzyme with specs addon

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,6 @@
 import { configure } from '@storybook/react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 //root styles
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,4 +12,5 @@ function loadStories() {
   req.keys().forEach((filename) => req(filename));
 }
 
+Enzyme.configure({ adapter: new Adapter() });
 configure(loadStories, module);
--- client/src/stories/loginForm.js
+++ client/src/stories/loginForm.js
@@ -4,10 +4,12 @@ import LoginForm from '../components/LoginForm';
 import { Container } from 'reactstrap';
 import '../containers/Login/Login.css';
 import { withInfo } from '@storybook/addon-info';
+import { specs, describe, it } from 'storybook-addon-specifications';
+import { mount } from 'enzyme';
 
 storiesOf('LoginForm', module).add(
-	'Login Form',
-	withInfo(`
+  'Login Form',
+  withInfo(`
       Statless reuseable login form component. In order to run this component with styles need to 
       wrap under Container with class login-container
       
@@ -16,9 +18,23 @@ storiesOf('LoginForm', module).add(
       <LoginForm />
       ~~~
     
-    `)(() => (
-		<Container className="login-container">
-			<LoginForm />
-		</Container>
-	))
+    `)(() => {
+    const story = (
+      <Container className="login-container">
+        {' '}
+        <LoginForm />{' '}
+      </Container>
+    );
+    spec(() =>
+      describe('LoginForm', () => {
+        test('simulates click event', () => {
+          const wrapper = mount(story);
+          wrapper.find('Button').simulate('click');
+          expect(onButtonClick.calledOnce).to.equal(true);
+        });
+      })
+    );
+
+    return story;
+  })
 );
