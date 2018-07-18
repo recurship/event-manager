d13833a0cd1c275e59796c9492c8aee8663e4b9d
saadabbasi <saad@marketlytics.com>
2018-07-18 10:48:41 +0500

    feat: intercept invalid-token responses and reform request

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,10 +1,10 @@
-
+// import {  } from './../actions';
+import { store } from '../../src';
+import { REFRESH_TOKEN, refreshToken } from '../actions';
 
 export const BASE_URL = ''; // or url for staging
-
-export const makeRequest = (path, options) => {
-
-  if(!options) {
+const makeFetchRequest = (path, options) => {
+  if (!options) {
     options = {};
   }
 
@@ -16,11 +16,27 @@ export const makeRequest = (path, options) => {
     if (!options.headers) {
       options.headers = {};
     }
-    options.headers['Authorization'] =  `Bearer ${ token }`;
+    options.headers['Authorization'] = `Bearer ${token}`;
   }
 
-  return fetch(BASE_URL + path, options).then((response) => {
+  return fetch(BASE_URL + path, options).then(response => {
     return response.json();
   });
+};
 
+export const makeRequest = async (path, options) => {
+  const response = await makeFetchRequest(path, options);
+  if (response.code && response.code == 'token_not_valid') {
+    const { refresh } = store.getState().userState,
+      formData = new FormData();
+    formData.append('refresh', refresh);
+
+    const body = { method: 'POST', body: formData },
+      refreshedToken = await makeFetchRequest('/api/token/refresh', body),
+      refreshTokenAction = refreshToken(refreshedToken);
+    store.dispatch(refreshTokenAction);
+    makeRequest(path, options);
+  } else {
+    return response;
+  }
 };
