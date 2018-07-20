5e6023d62c4c369d2444fc2d602cb552c1678e86
Talia Asghar Mirza <talia@marketlytics.com>
2018-07-20 12:30:41 +0500

    organization page redesigned

--- client/src/containers/Organisation/Organisation.js
+++ client/src/containers/Organisation/Organisation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Jumbotron, Button } from 'reactstrap';
+import { Container, Row, Col, CardImg, Button } from 'reactstrap';
 import { EventList } from '../../components/EventList/EventList';
+import ContentHeader from '../../components/ContentHeader/ContentHeader';
 
 class Organisation extends Component {
   constructor() {
@@ -13,7 +14,7 @@ class Organisation extends Component {
       name: 'GDG Kolachi',
       is_active: false,
       logo:
-        'http://diylogodesigns.com/blog/wp-content/uploads/2016/04/google-logo-icon-PNG-Transparent-Background.png',
+        'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Logo.png',
       description:
         'The biggest Google tech conference in Karachi. All about Android, Web, and Cloud.',
       owner: {
@@ -58,21 +59,38 @@ class Organisation extends Component {
     this.setState({ organisation: mockOrganisation });
   }
   render() {
-    console.log(this.state);
     return (
       <div>
-        <Jumbotron>
-          <h3>{this.state.organisation.name}</h3>
-          <h6>{this.state.organisation.description}</h6>
-          <Button className="btn btn-success">Subscribe</Button>
-        </Jumbotron>
-        {this.state.organisation.events ? (
-          <EventList events={this.state.organisation.events} />
-        ) : (
-          <Container>
-            <h4 className="text-center">Not Any Event Yet</h4>
-          </Container>
-        )}
+        <Container>
+          <CardImg top width="100%" src={this.state.organisation.logo} />
+          <ContentHeader heading={this.state.organisation.name} />
+          <Row className="block-content text-justify">
+            <Col>
+              <strong>{this.state.organisation.description}</strong>
+              This HTML file is a template. If you open it directly in the
+              browser, you will see an empty page. You can add webfonts, meta
+              tags, or analytics to this file. The build step will place the
+              bundled scripts into the tag. To begin the development, run `npm
+              start` or `yarn start`. To create a production bundle, use `npm
+              run build` or `yarn build`.<br />
+              <center>
+                <Button className="btn btn-success my-3">Subscribe</Button>
+              </center>
+            </Col>
+          </Row>
+          <ContentHeader heading="Our Events" />
+          <Row className="block-content text-justify">
+            <Col>
+              {this.state.organisation.events ? (
+                <EventList events={this.state.organisation.events} />
+              ) : (
+                <Container>
+                  <h4 className="text-center">Not Any Event Yet</h4>
+                </Container>
+              )}
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
