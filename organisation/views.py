fb1103bd58c50b93939e1d46bd7ee66790e44f0a
usman-hussain <usmanh@marketlytics.com>
2018-07-23 17:28:34 +0500

    get organisation with events

--- organisation/views.py
+++ organisation/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Organisation
+from event.models import Event
+from event.serializers import EventSerializer
 from .serializer import OrganisationSerializer, OrganisationCreateSerializer
 # Create your views here.
-
+from rest_framework.response import Response
+from django.http import Http404
 
 class OrganisationView(viewsets.ModelViewSet):
 
@@ -14,3 +17,15 @@ class OrganisationView(viewsets.ModelViewSet):
         if self.request.method == 'POST':
             return OrganisationCreateSerializer
         return OrganisationSerializer
+
+    def retrieve(self,request,pk=None):
+        try:
+            queryset_organisation = Organisation.objects.get(pk=pk)
+        except Organisation.DoesNotExist:
+            raise Http404
+        queryset_event = Event.objects.filter(organisation=pk).values()
+        serialized_data_organisation = OrganisationSerializer(queryset_organisation)
+        data = { **serialized_data_organisation.data }
+        data['events'] = queryset_event
+
+        return Response(data, status=status.HTTP_200_OK)
