b86538f4c2288ed12884c94a5da5eec93a659652
Qaseem Wakeel Lodhi <qaseemlodhi93@gmail.com>
2018-07-24 18:20:23 +0500

    fix(frontend): fix routing issue

--- client/src/containers/App/PublicContainer.js
+++ client/src/containers/App/PublicContainer.js
@@ -5,7 +5,7 @@ import Events from '../Events/Events';
 import { EMNavbar } from '../../components/EMNavbar';
 import type { BaseReduxPropTypes } from '../../types/base-props-types';
 import { Container } from 'reactstrap';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import EventDetails from '../EventDetails/EventDetails';
 import Organisation from '../Organisation/Organisation';
 import { connect } from 'react-redux';
@@ -15,35 +15,37 @@ type Props = {};
 class PublicContainer extends Component<Props> {
   render() {
     return (
-      <Fragment>
-        <Route
-          exact
-          render={() => (
-            <div>
-              <EMNavbar userData={this.props} /> <Events />
-            </div>
-          )}
-          path="/events"
-        />
-        <Route
-          exact
-          render={props => (
-            <div>
-              <EMNavbar userData={this.props} /> <EventDetails {...props} />
-            </div>
-          )}
-          path="/events/:event_id"
-        />
-        <Route
-          exact
-          render={props => (
-            <div>
-              <EMNavbar userData={this.props} /> <Organisation {...props} />
-            </div>
-          )}
-          path="/organisations/:organisation_id"
-        />
-      </Fragment>
+      <Router>
+        <Fragment>
+          <Route
+            exact
+            render={() => (
+              <div>
+                <EMNavbar userData={this.props} /> <Events />
+              </div>
+            )}
+            path="/events"
+          />
+          <Route
+            exact
+            render={props => (
+              <div>
+                <EMNavbar userData={this.props} /> <EventDetails {...props} />
+              </div>
+            )}
+            path="/events/:event_id"
+          />
+          <Route
+            exact
+            render={props => (
+              <div>
+                <EMNavbar userData={this.props} /> <Organisation {...props} />
+              </div>
+            )}
+            path="/organisations/:organisation_id"
+          />
+        </Fragment>
+      </Router>
     );
   }
 }
