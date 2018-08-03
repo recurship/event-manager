4867ea39fa5d81e5ccfd85a251b04004b3c35f90
Javaeria <javerianisar.1993@gmail.com>
2018-08-03 15:43:37 +0500

    feat: edit api working

--- client/src/containers/UserProfile/UserProfile.js
+++ client/src/containers/UserProfile/UserProfile.js
@@ -4,6 +4,9 @@ import { Button } from 'reactstrap';
 import { fetchUserProfile } from '../../actions';
 import Profile from './../../components/UserProfile/UserProfile';
 import { AttendeeType } from '../../types/attendee-types';
+import { Link } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import { store } from '../../../src';
 
 class UserProfile extends Component {
   constructor(props) {
@@ -22,10 +25,24 @@ class UserProfile extends Component {
     return user ? user : null;
   }
 
+  showEditButton = user => {
+    const { token } = store.getState().userState;
+    const payloadUserId = jwtDecode(token).user_id;
+    return token && payloadUserId == user.id ? (
+      <Link to={`/users/${user.id}/edit`}>
+        <Button id="edit-user" className="btn btn-default">
+          <span className="fa fa-edit" />
+        </Button>
+      </Link>
+    ) : null;
+  };
+
   render() {
     const { user } = this.getUserDetails();
+
     return user ? (
       <div style={{ marginTop: '20px' }}>
+        <div className="float-right mx-5">{this.showEditButton(user)}</div>
         <Profile user={user} />
       </div>
     ) : null;
