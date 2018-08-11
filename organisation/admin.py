5a1d57a47e64c12936155c158dcb40ce89c9fe85
usman-hussain <usmanh@marketlytics.com>
2018-08-11 16:35:58 +0500

    user acess update for admin panel

--- organisation/admin.py
+++ organisation/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 # Register your models here.
 from organisation.models import Organisation
+from user.models import User
 
-admin.site.register(Organisation)
+
+class OrganisationAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            return Organisation.objects.all()
+        return Organisation.objects.filter(owner=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # when we are creating or editing new event and the logged in user is not superuser
+        # so we need to fetch only those organisations which owner is request.user
+        if db_field.name == "owner" and not request.user.is_superuser:
+            kwargs["queryset"] = User.objects.filter(id=request.user.id)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Organisation, OrganisationAdmin)
