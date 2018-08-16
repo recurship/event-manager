bf069d6c67cbb1ccb4e210ce924702b67650669c
Zainab Saleem <zainab@marketlytics.com>
2018-08-16 12:16:27 +0500

    Added more details to organisation

--- organisation/models.py
+++ organisation/models.py
@@ -9,6 +9,10 @@ class Organisation(models.Model):
     description = models.TextField(null=True)
     is_active = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(db_index=True, unique=True, blank=False)
+    contact_number = models.CharField(max_length=11, blank=True)
+    facebook = models.URLField(max_length=255, blank=True)
+    twitter = models.URLField(max_length=255, blank=True)
     logo = models.FileField(upload_to='organisation/', blank=False, null=True,
                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png', 'jpeg'])])
 
