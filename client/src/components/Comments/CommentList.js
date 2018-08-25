577ae81495d8117fb8c2938a5dd87da798350f17
Talia Asghar <taley.mirza@gmail.com>
2018-08-25 12:30:05 +0500

    [style] improve design and structure
    - remove a package
    - improve css for avatar
    - improve responsiveness

--- client/src/components/Comments/CommentList.js
+++ client/src/components/Comments/CommentList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Col, Row } from 'reactstrap';
-import TimeAgo from 'react-timeago';
+import moment from 'moment';
 
 class CommentList extends Component {
   render() {
@@ -17,7 +17,7 @@ class CommentList extends Component {
               &times;
             </button>
             <Row>
-              <Col sm="2" md="1" xs="2">
+              <Col sm="4" md="2" xs="4" lg="2">
                 <img
                   className="commenterImage"
                   src={
@@ -27,11 +27,11 @@ class CommentList extends Component {
                   }
                 />
               </Col>
-              <Col sm="10" md="9" xs="10" className="commentText">
+              <Col sm="8" md="10" xs="8" lg="10" className="commentText">
                 <strong>{comment.commentedBy.username}</strong>
                 <p>{comment.comment}</p>
                 <span className="date sub-text">
-                  <TimeAgo date={comment.commentDatetime} />
+                  {moment(comment.commentDatetime).fromNow()}
                 </span>
               </Col>
             </Row>
