210d92e581bf7463bd1829064a9b5c545c83403d
Talia Asghar <taley.mirza@gmail.com>
2018-08-25 11:33:27 +0500

    [fix] resolve comments and fix a bug

--- client/src/containers/App/routes/private.routes.js
+++ client/src/containers/App/routes/private.routes.js
@@ -1,36 +1,30 @@
 import React, { Fragment, Component } from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 //components
 import EditUserProfile from '../../EditProfile/EditUserProfile';
 
-const BaseRedirection = () => <Redirect to="/events" />;
-
 const LoginRedirection = () => <Redirect to="/login" />;
 
 const PrivateRoutes = props => (
   <Fragment>
-    <Switch>
-      <Route
-        path="/users/:user_id/edit"
-        render={routesProps => (
-          <div>
-            {props.userState.token ? (
-              <EditUserProfile {...routesProps} />
-            ) : (
-              <LoginRedirection />
-            )}
-          </div>
-        )}
-      />
-      <Route component={BaseRedirection} />
-    </Switch>
+    <Route
+      path="/users/:user_id/edit"
+      render={routesProps => (
+        <div>
+          {props.userState.token ? (
+            <EditUserProfile {...routesProps} />
+          ) : (
+            <LoginRedirection />
+          )}
+        </div>
+      )}
+    />
   </Fragment>
 );
 
 const mapStateToProps = state => {
-  // const { appState, userState, events } = state;
   return state;
 };
 
