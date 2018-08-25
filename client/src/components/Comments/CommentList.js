63410656c5f02bafbe6d582ccdf1055b454dc3e7
Talia Asghar <taley.mirza@gmail.com>
2018-08-25 13:01:39 +0500

    [improve] sub-division of CommentList

--- client/src/components/Comments/CommentList.js
+++ client/src/components/Comments/CommentList.js
@@ -1,41 +1,11 @@
 import React, { Component } from 'react';
-import { Col, Row } from 'reactstrap';
-import moment from 'moment';
-
+import CommentListItem from './CommentListItem';
 class CommentList extends Component {
   render() {
     return (
       <ul className="commentList">
         {this.props.comments.map(comment => (
-          <li key={comment.id}>
-            <button
-              type="button"
-              className="close"
-              title="delete"
-              aria-hidden="true"
-            >
-              &times;
-            </button>
-            <Row>
-              <Col sm="4" md="2" xs="4" lg="2">
-                <img
-                  className="commenterImage"
-                  src={
-                    comment.commentedBy.avatar
-                      ? comment.commentedBy.avatar
-                      : 'http://placekitten.com/50/50'
-                  }
-                />
-              </Col>
-              <Col sm="8" md="10" xs="8" lg="10" className="commentText">
-                <strong>{comment.commentedBy.username}</strong>
-                <p>{comment.comment}</p>
-                <span className="date sub-text">
-                  {moment(comment.commentDatetime).fromNow()}
-                </span>
-              </Col>
-            </Row>
-          </li>
+          <CommentListItem comment={comment} />
         ))}
       </ul>
     );
