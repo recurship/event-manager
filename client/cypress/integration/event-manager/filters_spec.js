f055909845c5c35a1b60f5a95a1cac6faf0760c7
faria-ejaz <fariaejaz@outlook.com>
2018-10-02 17:32:02 +0500

    [test] Oraganisation filter

--- client/cypress/integration/event-manager/filters_spec.js
+++ client/cypress/integration/event-manager/filters_spec.js
@@ -14,7 +14,50 @@ describe('Filters Testing', () => {
   });
   context('Test for Organization Filters', () => {
     it('should have an arrow button', () => {
-      cy.get('div .Select #react-select-2--value').click()
-    });  
+      cy.get(
+        ':nth-child(1)  .Select  .Select-control  .Select-arrow-zone  .Select-arrow',
+        { timeout: 30000 }
+      ) //Gets the location of the Organisation field with a delay of 30000
+        .eq(0);
+      // .click(); //Clicks the toggle button
+    });
+
+    it('should select one option', () => {
+      cy.get(
+        ':nth-child(1)  .Select  .Select-control  .Select-arrow-zone  .Select-arrow',
+        { timeout: 30000 } //gets the location with a delay of 30000
+      )
+        .eq(0)
+        .click();
+      cy.contains('NES').click(); //Selects the dropdown menu option
+    });
+
+    it('should deselect option', () => {
+      cy.get(':nth-child(1)>.Select-value-icon').click(); //Deselects one of the option
+    });
+
+    it('should select multiple options', () => {
+      cy.get(
+        ':nth-child(1)  .Select  .Select-control  .Select-arrow-zone  .Select-arrow',
+        { timeout: 30000 } //gets the location with a delay of 30000
+      )
+        .eq(0)
+        .click();
+      cy.contains('NES').click(); //Selects the dropdown menu option
+      cy.get(
+        ':nth-child(1)  .Select  .Select-control  .Select-arrow-zone  .Select-arrow',
+        { timeout: 30000 } //gets the location with a delay of 30000
+      )
+        .eq(0)
+        .click();
+      cy.contains('Recurship').click(); //Selects the dropdown menu option
+    });
+
+    it('should deselect all the options', () => {
+      cy.get(':nth-child(1)>.Select-value-icon').click(); //Deselects one of the option
+      cy.get('.Select-value-icon').click(); //Deselects both
+    });
+
+    //    cy.get('form .btn').click();
   });
 });
