3e20ee515985b81ec004c5b29f3bb1f872372bbb
usman-hussain <usmanh@marketlytics.com>
2018-11-24 15:51:58 +0500

    password issue fixes

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,43 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.forms import PasswordInput
 
 from user.models import User
 
-admin.site.register(User)
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+        widgets = {
+            'password': PasswordInput(),
+        }
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    list_display = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {'fields': ('password', 'last_login', 'is_superuser', 'groups', 'user_permissions', 'username', 'email', \
+        'enable_notifications', 'first_name', 'last_name', 'is_active', 'is_staff', 'avatar')}),
+        )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'is_active')}
+            ),
+        )
+
+    filter_horizontal = ()
+
+admin.site.register(User, CustomUserAdmin)
