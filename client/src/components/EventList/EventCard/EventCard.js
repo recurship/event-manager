e5e3b5c776db0dffdbbeae10947b364eaf553fdd
faria ejaz <fejaz@sastaticket.pk>
2018-12-08 16:36:17 +0500

    Revert "[WIP] redesigning home page"
    This reverts commit d36dae83e2fcb108ade3309873360a3b5637d9f0.

--- client/src/components/EventList/EventCard/EventCard.js
+++ client/src/components/EventList/EventCard/EventCard.js
@@ -11,31 +11,22 @@ import {
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 
-const DATE_FORMAT = 'LL';
-const TIME_FORMAT = 'HH.A';
+const DATE_FORMAT = 'LLLL';
 const EventCard = props => {
   const card = props.event;
   return (
-    <Col className="cardFormating col-padding">
+    <Col md="4" className="col-padding">
       <Link to={`/events/${props.event.id}`}>
-        <Card className="bgGradient">
+        <Card>
           <CardImg
             top
             width="100%"
             src={card.cover}
             alt="Card image cap"
             height="180"
-            className="mx-auto pt-3 cardFormating"
           />
-          <CardBody className="mx-auto cardFormating my-3 bg-white">
+          <CardBody>
             <CardTitle>{card.title}</CardTitle>
-            <span className="text-muted ">
-              {moment(card.startDatetime).format(TIME_FORMAT)}
-            </span>{' '}
-            - &nbsp;
-            <span className="text-muted">
-              {moment(card.endDatetime).format(TIME_FORMAT)}
-            </span>
             <CardText>{card.description}</CardText>
             <CardSubtitle>
               <small className="text-muted">
--- client/src/containers/Events/Events.js
+++ client/src/containers/Events/Events.js
@@ -10,15 +10,7 @@ import { EventList } from '../../components/EventList/EventList';
 import DropSearch, {
   State as DropSearchState,
 } from '../../components/DropSearch/DropSearch';
-import EventCard from '../../components/EventList/EventCard/EventCard';
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-} from 'reactstrap';
+
 const mainTitle = 'Portal for Tech Communities';
 const eventNotFound = 'No events found.';
 
@@ -31,86 +23,92 @@ class Events extends Component<Props> {
   componentDidMount() {
     this.getData();
   }
-  // getSortyByOptions = () => {
-  //   return [
-  //     {
-  //       label: 'Start Date',
-  //       value: 'startDate',
-  //     },
-  //     {
-  //       label: 'Organisation',
-  //       value: 'organisation',
-  //     },
-  //     {
-  //       label: 'Sponsors',
-  //       value: 'sponsers',
-  //     },
-  //     {
-  //       label: 'Location',
-  //       value: 'location',
-  //     },
-  //     {
-  //       label: 'Tags',
-  //       value: 'tags',
-  //     },
-  //     {
-  //       label: 'Time',
-  //       value: 'time',
-  //     },
-  //   ];
-  // };
+  getSortyByOptions = () => {
+    return [
+      {
+        label: 'Start Date',
+        value: 'startDate',
+      },
+      {
+        label: 'Organisation',
+        value: 'organisation',
+      },
+      {
+        label: 'Sponsors',
+        value: 'sponsers',
+      },
+      {
+        label: 'Location',
+        value: 'location',
+      },
+      {
+        label: 'Tags',
+        value: 'tags',
+      },
+      {
+        label: 'Time',
+        value: 'time',
+      },
+    ];
+  };
 
-  // handleSearchChange = (searchParams: DropSearchState) => {
-  //   if (searchParams) searchParams = makeQueryStringTransformable(searchParams);
-  //   this.getData(searchParams);
-  // };
+  handleSearchChange = (searchParams: DropSearchState) => {
+    if (searchParams) searchParams = makeQueryStringTransformable(searchParams);
+    this.getData(searchParams);
+  };
 
   getData = e => {
     const { dispatch } = this.props;
     dispatch(fetchEvents(e));
   };
 
-  // handleSubmit = (event: any) => {
-  //   event.preventDefault();
+  handleSubmit = (event: any) => {
+    event.preventDefault();
 
-  //   const {
-  //     title,
-  //     description,
-  //     startDateTime,
-  //     endDateTime,
-  //     organisation,
-  //   } = event.nativeEvent.target.elements;
+    const {
+      title,
+      description,
+      startDateTime,
+      endDateTime,
+      organisation,
+    } = event.nativeEvent.target.elements;
 
-  //   this.props.dispatch(
-  //     postEvent({
-  //       title: title.value,
-  //       description: description.value,
-  //       startDateTime: startDateTime.value,
-  //       endDateTime: endDateTime.value,
-  //       organisation: organisation.value,
-  //     })
-  //   );
-  // };
+    this.props.dispatch(
+      postEvent({
+        title: title.value,
+        description: description.value,
+        startDateTime: startDateTime.value,
+        endDateTime: endDateTime.value,
+        organisation: organisation.value,
+      })
+    );
+  };
 
   render() {
     const { events } = this.props;
     return (
       <div>
-        <Col className="cardFormating mx-auto py-5 mt-4 bg-dark">
+        <Jumbotron>
+          <h3 className="text-center">{mainTitle}</h3>
+        </Jumbotron>
+        <Container>
+          <Row>
+            <Col md="12">
+              <DropSearch
+                sortBy={this.getSortyByOptions()}
+                handleSearchChange={this.handleSearchChange}
+                events={events}
+              />
+            </Col>
+          </Row>
+        </Container>
+        {events.events.length ? (
+          <EventList events={events.events} />
+        ) : (
           <Container>
-            <Row>
-              <Col md="20" className="mx-auto mt-3">
-                {events.events.length ? (
-                  <EventList events={events.events} />
-                ) : (
-                  <Container>
-                    <h4 className="text-center">{eventNotFound}</h4>
-                  </Container>
-                )}
-              </Col>
-            </Row>
+            <h4 className="text-center">{eventNotFound}</h4>
           </Container>
-        </Col>
+        )}
       </div>
     );
   }
