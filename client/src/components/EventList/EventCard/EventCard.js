67908905df8345503a61c622d50f073bf6a3533e
Qaseem Wakeel Lodhi <qaseemlodhi93@gmail.com>
2018-12-08 16:29:09 +0500

    truncate event detail on home page.
    https://github.com/recurship/event-manager/issues/195

--- client/src/components/EventList/EventCard/EventCard.js
+++ client/src/components/EventList/EventCard/EventCard.js
@@ -11,13 +11,19 @@ import {
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 
+import './EventCard.css';
+
 const DATE_FORMAT = 'LLLL';
 const EventCard = props => {
   const card = props.event;
+
+  const startingDesc = card.description.slice(0, 35);
+  const remainingDesc = card.description.slice(35, card.description.length);
+
   return (
     <Col md="4" className="col-padding">
-      <Link to={`/events/${props.event.id}`}>
-        <Card>
+      <Card>
+        <Link to={`/events/${props.event.id}`}>
           <CardImg
             top
             width="100%"
@@ -25,17 +31,27 @@ const EventCard = props => {
             alt="Card image cap"
             height="180"
           />
-          <CardBody>
-            <CardTitle>{card.title}</CardTitle>
-            <CardText>{card.description}</CardText>
-            <CardSubtitle>
-              <small className="text-muted">
-                {moment(card.startDatetime).format(DATE_FORMAT)}
-              </small>
-            </CardSubtitle>
-          </CardBody>
-        </Card>
-      </Link>
+        </Link>
+        <CardBody>
+          <CardTitle>{card.title}</CardTitle>
+          {startingDesc.length === 35 && (
+            <input type="checkbox" class="read-more-state" id={card.id} />
+          )}
+          <CardText className="read-more-wrap">
+            {startingDesc}
+            {startingDesc.length === 35 && (
+              <span className="dotspan">&nbsp;...</span>
+            )}
+            <span className="read-more-target">{remainingDesc}</span>
+          </CardText>
+          <label htmlFor={card.id} className="read-more-trigger" />
+          <CardSubtitle>
+            <small className="text-muted">
+              {moment(card.startDatetime).format(DATE_FORMAT)}
+            </small>
+          </CardSubtitle>
+        </CardBody>
+      </Card>
     </Col>
   );
 };
