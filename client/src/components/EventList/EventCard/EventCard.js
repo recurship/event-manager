d36dae83e2fcb108ade3309873360a3b5637d9f0
faria ejaz <fejaz@sastaticket.pk>
2018-12-08 16:31:33 +0500

    [WIP] redesigning home page

--- client/src/components/EventList/EventCard/EventCard.js
+++ client/src/components/EventList/EventCard/EventCard.js
@@ -11,22 +11,31 @@ import {
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 
-const DATE_FORMAT = 'LLLL';
+const DATE_FORMAT = 'LL';
+const TIME_FORMAT = 'HH.A';
 const EventCard = props => {
   const card = props.event;
   return (
-    <Col md="4" className="col-padding">
+    <Col className="cardFormating col-padding">
       <Link to={`/events/${props.event.id}`}>
-        <Card>
+        <Card className="bgGradient">
           <CardImg
             top
             width="100%"
             src={card.cover}
             alt="Card image cap"
             height="180"
+            className="mx-auto pt-3 cardFormating"
           />
-          <CardBody>
+          <CardBody className="mx-auto cardFormating my-3 bg-white">
             <CardTitle>{card.title}</CardTitle>
+            <span className="text-muted ">
+              {moment(card.startDatetime).format(TIME_FORMAT)}
+            </span>{' '}
+            - &nbsp;
+            <span className="text-muted">
+              {moment(card.endDatetime).format(TIME_FORMAT)}
+            </span>
             <CardText>{card.description}</CardText>
             <CardSubtitle>
               <small className="text-muted">
