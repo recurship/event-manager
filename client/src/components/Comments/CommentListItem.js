1ca9bef8674af77aabb1847be9744a3e7215c68d
Talia Asghar <taley.mirza@gmail.com>
2018-12-08 15:25:08 +0500

    [feat] add edit and delete dropdown in comment

--- client/src/components/Comments/CommentListItem.js
+++ client/src/components/Comments/CommentListItem.js
@@ -1,21 +1,84 @@
 import React, { Component } from 'react';
-import { Col, Row } from 'reactstrap';
+import {
+  Col,
+  Row,
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
 import moment from 'moment';
 import avatar from '../../../src/avatar.jpg';
 
 class CommentListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpen: false,
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  };
   render() {
     const comment = this.props.comment;
     return (
       <li key={comment.id}>
-        <button
-          type="button"
-          className="close"
-          title="delete"
-          aria-hidden="true"
-        >
-          &times;
-        </button>
+        <div className="settings-btn">
+          <ButtonDropdown
+            isOpen={this.state.dropdownOpen}
+            toggle={this.toggle}
+            className="comment-options"
+          >
+            <DropdownToggle className="dots-btn">
+              <i className="fa fa-ellipsis-v" aria-hidden="true" />
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                <i className="fa fa-pencil fa-fw" /> Edit
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                <i className="fa fa-trash-o fa-fw" /> Delete
+              </DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+        </div>
+        {/* <div className="btn-group open">
+          <a
+            className="btn btn-primary dropdown-toggle"
+            data-toggle="dropdown"
+            href="#"
+          >
+k          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a href="#">
+                <i className="fa fa-pencil fa-fw" /> Edit
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-trash-o fa-fw" /> Delete
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-ban fa-fw" /> Ban
+              </a>
+            </li>
+            <li className="divider" />
+            <li>
+              <a href="#">
+                <i className="fa fa-unlock" /> Make admin
+              </a>
+            </li>
+          </ul>
+        </div> */}
         <Row>
           <Col sm="4" md="2" xs="4" lg="2">
             <img
