f00f7203d0253e0a208ccbc0a2e14e0302d5269b
faria ejaz <fejaz@sastaticket.pk>
2018-12-08 16:37:59 +0500

    Revert "Revert "[WIP] redesigning home page""
    This reverts commit e5e3b5c776db0dffdbbeae10947b364eaf553fdd.

--- client/src/containers/Events/Events.js
+++ client/src/containers/Events/Events.js
@@ -10,7 +10,15 @@ import { EventList } from '../../components/EventList/EventList';
 import DropSearch, {
   State as DropSearchState,
 } from '../../components/DropSearch/DropSearch';
-
+import EventCard from '../../components/EventList/EventCard/EventCard';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from 'reactstrap';
 const mainTitle = 'Portal for Tech Communities';
 const eventNotFound = 'No events found.';
 
@@ -23,92 +31,86 @@ class Events extends Component<Props> {
   componentDidMount() {
     this.getData();
   }
-  getSortyByOptions = () => {
-    return [
-      {
-        label: 'Start Date',
-        value: 'startDate',
-      },
-      {
-        label: 'Organisation',
-        value: 'organisation',
-      },
-      {
-        label: 'Sponsors',
-        value: 'sponsers',
-      },
-      {
-        label: 'Location',
-        value: 'location',
-      },
-      {
-        label: 'Tags',
-        value: 'tags',
-      },
-      {
-        label: 'Time',
-        value: 'time',
-      },
-    ];
-  };
+  // getSortyByOptions = () => {
+  //   return [
+  //     {
+  //       label: 'Start Date',
+  //       value: 'startDate',
+  //     },
+  //     {
+  //       label: 'Organisation',
+  //       value: 'organisation',
+  //     },
+  //     {
+  //       label: 'Sponsors',
+  //       value: 'sponsers',
+  //     },
+  //     {
+  //       label: 'Location',
+  //       value: 'location',
+  //     },
+  //     {
+  //       label: 'Tags',
+  //       value: 'tags',
+  //     },
+  //     {
+  //       label: 'Time',
+  //       value: 'time',
+  //     },
+  //   ];
+  // };
 
-  handleSearchChange = (searchParams: DropSearchState) => {
-    if (searchParams) searchParams = makeQueryStringTransformable(searchParams);
-    this.getData(searchParams);
-  };
+  // handleSearchChange = (searchParams: DropSearchState) => {
+  //   if (searchParams) searchParams = makeQueryStringTransformable(searchParams);
+  //   this.getData(searchParams);
+  // };
 
   getData = e => {
     const { dispatch } = this.props;
     dispatch(fetchEvents(e));
   };
 
-  handleSubmit = (event: any) => {
-    event.preventDefault();
+  // handleSubmit = (event: any) => {
+  //   event.preventDefault();
 
-    const {
-      title,
-      description,
-      startDateTime,
-      endDateTime,
-      organisation,
-    } = event.nativeEvent.target.elements;
+  //   const {
+  //     title,
+  //     description,
+  //     startDateTime,
+  //     endDateTime,
+  //     organisation,
+  //   } = event.nativeEvent.target.elements;
 
-    this.props.dispatch(
-      postEvent({
-        title: title.value,
-        description: description.value,
-        startDateTime: startDateTime.value,
-        endDateTime: endDateTime.value,
-        organisation: organisation.value,
-      })
-    );
-  };
+  //   this.props.dispatch(
+  //     postEvent({
+  //       title: title.value,
+  //       description: description.value,
+  //       startDateTime: startDateTime.value,
+  //       endDateTime: endDateTime.value,
+  //       organisation: organisation.value,
+  //     })
+  //   );
+  // };
 
   render() {
     const { events } = this.props;
     return (
       <div>
-        <Jumbotron>
-          <h3 className="text-center">{mainTitle}</h3>
-        </Jumbotron>
-        <Container>
-          <Row>
-            <Col md="12">
-              <DropSearch
-                sortBy={this.getSortyByOptions()}
-                handleSearchChange={this.handleSearchChange}
-                events={events}
-              />
-            </Col>
-          </Row>
-        </Container>
-        {events.events.length ? (
-          <EventList events={events.events} />
-        ) : (
+        <Col className="cardFormating mx-auto py-5 mt-4 bg-dark">
           <Container>
-            <h4 className="text-center">{eventNotFound}</h4>
+            <Row>
+              <Col md="20" className="mx-auto mt-3">
+                {events.events.length ? (
+                  <EventList events={events.events} />
+                ) : (
+                  <Container>
+                    <h4 className="text-center">{eventNotFound}</h4>
+                  </Container>
+                )}
+              </Col>
+            </Row>
           </Container>
-        )}
+        </Col>
       </div>
     );
   }
