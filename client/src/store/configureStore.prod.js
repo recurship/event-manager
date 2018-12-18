580a5b6adb5cf89dbf6d5f6fe7a222064482a833
shahnawaz19415 <shahnawaz19415@gmail.com>
2018-12-18 15:02:37 +0500

    Redux form validation test in Login

--- client/src/store/configureStore.prod.js
+++ client/src/store/configureStore.prod.js
@@ -13,4 +13,6 @@ const configureStore = preloadedState =>
     )
   );
 
-export default configureStore();
+const store = configureStore();
+
+export default store;
--- client/src/test/login.test.js
+++ client/src/test/login.test.js
@@ -1,6 +1,7 @@
 // Login Form Full DOM Rendering Test
 import React from 'react';
 import Login from '../containers/Login/Login';
+import LoginForm from '../components/LoginForm/LoginForm';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import store from '../store/configureStore';
@@ -29,15 +30,29 @@ describe('<Login />', () => {
     expect(wrapper.find('form').length).toEqual(1);
   });
 
-  it('Component should trigger login', () => {
+  it('Should check if empty form is invalid', () => {
+    expect(
+      wrapper.find(LoginForm).instance().ref.wrappedInstance.props.valid
+    ).toEqual(false);
+  });
+
+  it('Should check if fields can be edited', () => {
     wrapper
       .find('input[name="username"]')
       .simulate('change', { target: { value: 'jack' } });
     wrapper
       .find('input[name="password"]')
       .simulate('change', { target: { value: '123456' } });
-    wrapper.find('form').simulate('submit');
+  });
 
+  it('Should check if filled form is valid', () => {
+    expect(
+      wrapper.find(LoginForm).instance().ref.wrappedInstance.props.valid
+    ).toEqual(true);
+  });
+
+  it('SHould check if login is triggered', () => {
+    wrapper.find('form').simulate('submit');
     expect(mockLogin).toHaveBeenCalledTimes(1);
 
     expect(mockLogin).toHaveBeenCalledWith({
